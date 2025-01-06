431f427fa5832406979dc10c2828dedd228fe87a
jolyssa <jolyssa.armstrong@gmail.com>
2025-01-05 23:06:44 -0500

    squiggly in hero, for deskstop

--- edu/src/components/Hero.jsx
+++ edu/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-const Hero = ({ subtitle = 'Explore the best resources, tutorials, and tools to level up your skills. Add a resource to share the wealth.'}) => {
+const Hero = ({ subtitle = 'Explore the best resources, tutorials, and tools to level up your skills. Add a resource to share the wealth.' }) => {
     return (
         <>
             {/* <!-- Hero --> */}
@@ -10,10 +10,21 @@ const Hero = ({ subtitle = 'Explore the best resources, tutorials, and tools to 
                         <h1
                             className="text-4xl font-extrabold text-white sm:text-5xl md:text-7xl"
                         >
-                            Your Coding Education Lives <span className="text-gray-800 underline">Here</span>
+                            Your Coding Education Lives {" "}
+                            <span className="relative inline-block">
+                                <span className="relative z-10 text-red-200">Here</span>
+                                <span
+                                    className="absolute inset-x-0 bottom-0 bg-no-repeat bg-contain w-80 h-80"
+                                    style={{
+                                        backgroundImage: "url('brush.png')",
+                                        bottom: '-230px',
+                                        left: '-68px',
+                                    }}
+                                ></span>
+                            </span>
                         </h1>
-                        <p className="mt-10 mb-4 text-xl text-white px-16">
-                            { subtitle }
+                        <p className="mt-20 mb-4 text-xl text-white px-16">
+                            {subtitle}
                         </p>
                     </div>
                 </div>
